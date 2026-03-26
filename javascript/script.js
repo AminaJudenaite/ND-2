d98b3fda0b7bd63be34be177c0b97f6f40f1572e
AminaJudenaite <amina.judenaite@stud.vilniustech.lt>
2026-03-26 20:45:59 +0200

    Surasyti kodo komentarai

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,13 @@
 
-const nuolaida = document.getElementById('nuolaida');
-if (nuolaida) {
-    nuolaida.addEventListener('click', function() {
+const nuolaida = document.getElementById('nuolaida'); /* randamas elementas su id nuolaida */
+if (nuolaida) { /* tikrina ar yra toks elementas */
+    nuolaida.addEventListener('click', function() { /* pridedamas veikimas paspaudus mygtuką */
 
-        let pasirinkimas = confirm("Ar norite gauti 10% nuolaidą pirmam kursui?");
-        if (pasirinkimas) {
-            alert("Puiku. Jūsų kodas: DOVANA10.");
-        } else {
-            alert("Negavot 10 % nuolaidos");
+        let pasirinkimas = confirm("Ar norite gauti 10% nuolaidą pirmam kursui?"); /* rodomas langas su tekstu ar nori nuolaidos, OK arba Cancel */
+        if (pasirinkimas) { /* jei pasirenkama "OK" */
+            alert("Puiku. Jūsų kodas: DOVANA10."); /* rodomas pranešimas su nuolaidos kodu */
+        } else { /* jei pasirenkama "Cancel" */
+            alert("Negavot 10 % nuolaidos"); /* rodomas pranešimas kad negavo nuolaidos */
         }
     });
 }
